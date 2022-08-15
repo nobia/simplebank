8638126ed6d486c2ca947ea3da688b00efc5a8e8
Liu Hao <nobia@icloud.com>
2022-08-15 10:29:07 +0800

    Add viper config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,9 @@ import (
 	"os"
 	"testing"
 
-	_ "github.com/lib/pq"
-)
+	"github.com/nobia/simplebank/util"
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://u1:pass1@localhost:5432/simple_bank?sslmode=disable"
+	_ "github.com/lib/pq"
 )
 
 var (
@@ -20,9 +17,12 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
